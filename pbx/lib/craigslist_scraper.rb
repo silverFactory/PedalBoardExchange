2251394b1d81efd6e42145b46633fa799333a51b
Will Studabaker <will.studabaker@gmail.com>
2020-11-12 19:32:16 +0000

    Pedal Class Basics

--- pbx/lib/craigslist_scraper.rb
+++ pbx/lib/craigslist_scraper.rb
@@ -1,7 +1,7 @@
 require 'open-uri'
 require 'nokogiri'
 require 'pry'
-
+require_relative 'pedal.rb'
 #Craigslist scrape params
     #post title: results.css("h3").text
     #Price:     results.css(".result-meta .result-price").text
@@ -10,13 +10,20 @@ class Scraper
   def self.scrape_search(pedal_type)
     html = open("https://newyork.craigslist.org/search/msa?query=#{pedal_type}")
     doc = Nokogiri::HTML(html)
-    results = doc.css(".result-info")
-    results.css(".result-meta")
+    results = doc.css(".result-row")
+    #puts results.length
+    results.each do |r|
+      pedal = Pedal.new
+      pedal.name = r.css("h3").text
+      pedal.seller_price = r.css(".result-meta .result-price").text
+    end
 
-    # results.each do |r|
-    #   puts r.css("a")
-    # end
+    # for each result, make a new pedal object with name and price
   end
 
 end
-puts Scraper.scrape_search("tube+screamer")
+Scraper.scrape_search("tube+screamer")
+#puts Pedal.all[0].name
+Pedal.all.each do |p|
+  puts "#{p.name}: #{p.seller_price}"
+end
--- pbx/lib/pedal.rb
+++ pbx/lib/pedal.rb
@@ -1,4 +1,12 @@
 class Pedal
-  attr_accessor :manufacturer, :model, :seller_price, :gc_price, :gc_description, :gc_used, :Ebay_buy_now, :for_trade
-
+  attr_accessor :name, :manufacturer, :model, :seller_price, :gc_price, :gc_description, :gc_used, :Ebay_buy_now, :for_trade
+  @@all = []
+  def initialize(name= nil, price = nil)
+    @name = name
+    @price = price
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
 end
