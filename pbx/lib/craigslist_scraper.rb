4442df7ad03e4e07efb9eed001e1ed5f8d2ac33c
Will Studabaker <will.studabaker@gmail.com>
2020-11-12 20:22:47 +0000

    GC Scrape Trouble

--- pbx/lib/craigslist_scraper.rb
+++ pbx/lib/craigslist_scraper.rb
@@ -17,13 +17,20 @@ class Scraper
       pedal.name = r.css("h3").text
       pedal.seller_price = r.css(".result-meta .result-price").text
     end
-
     # for each result, make a new pedal object with name and price
   end
 
+  def self.gc_scrape(pedal_name)
+    html =open("https://www.guitarcenter.com/Ibanez/TS9-Tube-Screamer-Effects-Pedal-1274115043047.gc")
+    binding.pry
+    doc = Nokogiri::HTML(html)
+    results = doc.css(".product")
+    puts doc
+  end
 end
-Scraper.scrape_search("tube+screamer")
+# Scraper.scrape_search("tube+screamer")
+Scraper.gc_scrape("ibanez+tube+screamer+mini")
 #puts Pedal.all[0].name
-Pedal.all.each do |p|
-  puts "#{p.name}: #{p.seller_price}"
-end
+# Pedal.all.each do |p|
+#   puts "#{p.name}: #{p.seller_price}"
+# end
--- pbx/lib/pedal.rb
+++ pbx/lib/pedal.rb
@@ -1,9 +1,7 @@
 class Pedal
   attr_accessor :name, :manufacturer, :model, :seller_price, :gc_price, :gc_description, :gc_used, :Ebay_buy_now, :for_trade
   @@all = []
-  def initialize(name= nil, price = nil)
-    @name = name
-    @price = price
+  def initialize
     @@all << self
   end
   def self.all
