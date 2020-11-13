fb30439cbd7972229f730e4849c5fb1d87ff06ed
Will Studabaker <will.studabaker@gmail.com>
2020-11-13 17:47:06 +0000

    deep scrape rework

--- pbx/lib/craigslist_scraper.rb
+++ pbx/lib/craigslist_scraper.rb
@@ -8,6 +8,20 @@ require 'pry'
 class Scraper
   MAJOR_CITIES = ["washingtondc", "baltimore", "philadelphia", "atlanta", "minneapolis", "portland", "cleveland", "boston", "denver", "austin", "losangeles"]
 
+  #redesign scraper so that it starts on https://www.craigslist.org/about/sites#US
+  # which contains the country broken up by state, and each state has 5-10 regions/cities
+  #and for each of these
+  def self.full_scrape(pedal_type)
+    html = open("https://www.craigslist.org/about/sites#US")
+    full_country = Nokogiri::HTML(html)
+    binding.pry
+    state_links = []
+    full_country.each do |area|
+      state_links << area.css("a").attribute("href").text
+    end
+      puts state_links
+  end
+
   def self.scrape_search(pedal_type, city = "newyork")
     html = open("https://#{city}.craigslist.org/search/msa?query=#{pedal_type}")
     doc = Nokogiri::HTML(html)
@@ -18,10 +32,10 @@ class Scraper
   #need method to cycle through initial results and only keep posts that have search params in title
     pedals = []
     results.each do |r|
-      if title_check?(r.css("h3").text, pedal_type)
+      if Scraper.title_check?(r.css("h3").text, pedal_type)
         pedal = {}
-        pedal[:name] = r.css("h3").text
-        pedal[:seller_price] = r.css(".result-meta .result-price").text
+        pedal[:name] = r.css("h3").text.strip
+        pedal[:seller_price] = r.css(".result-meta .result-price").text.strip
         pedals << pedal
       end
     end
@@ -37,39 +51,52 @@ class Scraper
       results = doc.css(".result-row")
         #method to cycle through initial results and only keep posts that have search params in title
       results.each do |r|
-        prices << r.css(".result-meta .result-price").text
+        prices << r.css(".result-meta .result-price").text.strip.split("$")[1].to_i
       end
     end
-    prices
+    reliable_prices = Scraper.price_range_limiter(prices)
+    average_price = Scraper.price_average(reliable_prices)
+    puts "Price average: $#{average_price}"
   end
 
-  def self.price_comparison(price_array)
+  def self.price_range_limiter(price_array)
     #throws out $0-$1 posts, finds mean and median
+    reliable_prices = []
+    price_array.each do |price|
+      #MAGIC NUMBERS BAD!!!!!
+      if price > 1 && price < 300
+        reliable_prices << price
+      end
+    end
+    reliable_prices
+  end
+
+  def self.price_average(price_array)
+    sum = 0
+    price_array.each{|p| sum += p}
+    sum / price_array.length
   end
 
   #method to cycle through initial results and only keep posts that have search params in title
-  def title_check?(post_title, search_param)
+  def self.title_check?(post_title, search_param)
     common_words = 0
     #break apart sear_param into individual words
-    search_param.downcase.split.each do |param|
+    parameters = search_param.downcase.split
+    parameters.each do |param|
     #downcase post_title and search word
       if post_title.downcase.include?(param)
         common_words += 1
       end
     end
     #return true if post_title contains at least one search param
-    if common_words >=1
+    if common_words == parameters.length
       true
     else
       false
     end
   end
-
-
+  #method to see if any posts have identical titles
+  def duplicate_check?()
+  end
 end
-# Scraper.scrape_search("tube+screamer")
-#Scraper.gc_scrape("ibanez+tube+screamer+mini")
-#puts Pedal.all[0].name
-# Pedal.all.each do |p|
-#   puts "#{p.name}: #{p.seller_price}"
-# end
+Scraper.full_scrape("tubescreamer")
--- pbx/lib/pbx.rb
+++ pbx/lib/pbx.rb
@@ -6,11 +6,17 @@ require 'pry'
 #   class Error < StandardError; end
 #   # Your code goes here...
 # end
-
-#OK testing my Git capabilities
+MAJOR_CITIES = ["washingtondc", "baltimore", "philadelphia", "atlanta",
+   "minneapolis", "portland", "cleveland", "boston", "denver", "austin",
+    "losangeles", "chicago", "dallas", "houston", "miami", "phoenix",
+    "sfbay", "detroit", "seattle", "sandiego", "tampa", "stlouis", "charlotte",
+  "orlando", "sanantonio", "sacramento", "pittsburgh", "lasvegas", ""]
 puts "Welcome to the PedalBoardExchange!!"
 puts "What are you searching for today?"
 input = gets.chomp
-pedal_results = Scraper.scrape_search(input)
-Pedal.create_from_collection(pedal_results)
+MAJOR_CITIES.each do |city|
+  pedal_results = Scraper.scrape_search(input, city)
+  Pedal.create_from_collection(pedal_results)
+end
 Pedal.all.each{|p| puts "#{p.name}: #{p.seller_price}"}
+#Scraper.country_scrape(input)
