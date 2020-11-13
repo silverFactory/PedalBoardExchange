651fc06a492edee7c88f751ebbf37fa9808a8637
Will Studabaker <will.studabaker@gmail.com>
2020-11-13 14:43:21 +0000

    #country search

--- pbx/lib/craigslist_scraper.rb
+++ pbx/lib/craigslist_scraper.rb
@@ -6,30 +6,44 @@ require_relative 'pedal.rb'
     #post title: results.css("h3").text
     #Price:     results.css(".result-meta .result-price").text
 class Scraper
+  MAJOR_CITIES = ["washingtondc", "baltimore", "philadelphia", "atlanta", "minneapolis", "portland", "cleveland", "boston", "denver", "austin", "losangeles"]
 
-  def self.scrape_search(pedal_type)
-    html = open("https://newyork.craigslist.org/search/msa?query=#{pedal_type}")
+  def self.scrape_search(pedal_type, city = "newyork")
+    html = open("https://#{newyork}.craigslist.org/search/msa?query=#{pedal_type}")
     doc = Nokogiri::HTML(html)
     results = doc.css(".result-row")
-    #puts results.length
+  # for each result, stoe info in a hash that pedal class uses to instantiate
+    pedals = []
     results.each do |r|
-      pedal = Pedal.new
-      pedal.name = r.css("h3").text
-      pedal.seller_price = r.css(".result-meta .result-price").text
+      pedal = {}
+      pedal[:name] = r.css("h3").text
+      pedal[:seller_price] = r.css(".result-meta .result-price").text
+      pedals << pedal
     end
-    # for each result, make a new pedal object with name and price
+    pedals
+  end
+  def self.country_scrape(pedal_type)
+    #takes in a pedal_type (same keyword as used for primary search) and finds similar posts in other major cities
+    #don't need to make objects out of results, just need to get price
+
+  end
+
+  def self.price_comparison()
+    #throws out $0-$1 posts, finds mean and median
   end
 
   def self.gc_scrape(pedal_name)
-    html =open("https://www.guitarcenter.com/Ibanez/TS9-Tube-Screamer-Effects-Pedal-1274115043047.gc")
-    binding.pry
+    html =open("https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313&_nkw=ibanez+tube+screamer&_sacat=0")
+    #binding.pry
     doc = Nokogiri::HTML(html)
-    results = doc.css(".product")
-    puts doc
+    results = doc.css(".product-card")
+    binding.pry
+
+    puts results
   end
 end
 # Scraper.scrape_search("tube+screamer")
-Scraper.gc_scrape("ibanez+tube+screamer+mini")
+#Scraper.gc_scrape("ibanez+tube+screamer+mini")
 #puts Pedal.all[0].name
 # Pedal.all.each do |p|
 #   puts "#{p.name}: #{p.seller_price}"
--- pbx/lib/pedal.rb
+++ pbx/lib/pedal.rb
@@ -1,8 +1,12 @@
 class Pedal
   attr_accessor :name, :manufacturer, :model, :seller_price, :gc_price, :gc_description, :gc_used, :Ebay_buy_now, :for_trade
   @@all = []
-  def initialize
+  def initialize(pedal_hash)
+    pedal_hash.each{|k, v| self.send("#{k}=", v)}
     @@all << self
+  end
+  def self.create_from_collection(pedals_array)
+    pedals_array.each{|p| Pedal.new(p)}
   end
   def self.all
     @@all
