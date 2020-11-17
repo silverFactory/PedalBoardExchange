06778ed5502ee7329e4732122c33a02db4cbd813
Will Studabaker <will.studabaker@gmail.com>
2020-11-17 19:46:13 +0000

    improved menu stability

--- pbx/lib/pbx.rb
+++ pbx/lib/pbx.rb
@@ -26,46 +26,57 @@ class PBX
     Pedal.all.each_with_index{|p, i| puts "#{i+1}- #{p.name}: listed for #{p.seller_price}"}
     puts "For more information about a pedal, please enter corresponding number:"
     ped_choice = gets.chomp.to_i
-    PBX.menu(ped_choice)
+    if ped_choice.between?(1, Pedal.all.length)
+      menu(ped_choice)
+    else
+      puts "Please enter a valid choice..."
+      listings
+    end
   end
   def self.menu(ped_choice)
-    puts Pedal.all[ped_choice-1].name
+    puts Pedal.all[ped_choice-1].name+" "+Pedal.all[ped_choice-1].seller_price
     puts "What would you like to know?"
     puts "1 - Seller Description"
     puts "2 - Price New as Listed on Guitar Center"
     puts "3 - Manufacturer Description"
     puts "4 - Back to Craigslist Search Results"
     sub_menu_choice = gets.chomp.to_i
-    PBX.sub_menu(sub_menu_choice, ped_choice)
+    if sub_menu_choice.between?(1, 4)
+      sub_menu(sub_menu_choice, ped_choice)
+    else
+      puts "Please enter a valid choice..."
+      menu(ped_choice)
+    end
   end
 
   def self.sub_menu(s_m_c, p_c)
     if s_m_c == 1
       puts Pedal.all[p_c-1].seller_description
-      PBX.end_menu(s_m_c, p_c)
+      end_menu(s_m_c, p_c)
     elsif s_m_c == 2
       puts Pedal.all[p_c-1].gc_price
-      PBX.end_menu(s_m_c, p_c)
+      end_menu(s_m_c, p_c)
     elsif s_m_c == 3
       puts Pedal.all[p_c-1].gc_description
-      PBX.end_menu(s_m_c, p_c)
+      end_menu(s_m_c, p_c)
     elsif s_m_c == 4
       self.listings
     else
       puts "Please enter a valid choice..."
+      sub_menu(s_m_c, p_c)
     end
   end
   def self.end_menu(s_m_c, p_c)
-    puts "\nWhat would you like to do next?"
+    puts "What would you like to do next?\n\n"
     puts "1 - Back to info about this pedal"
     puts "2 - Back to list of all pedals"
-    puts "3 - Exit program"
+    puts "Any Key - Exit program"
     end_menu = gets.chomp.to_i
     if end_menu == 1
-      PBX.menu(p_c)
+      menu(p_c)
     elsif end_menu == 2
-      PBX.listings
-    elsif end_menu == 3
+      listings
+    else 
       puts "Thanks for visiting PBX"
     end
   end
